655074946c1d33e5948ceaf7a40876eba84d3f4d
mostafasasa010 <mostafa.sasa20004@gmail.com>
2024-03-20 05:15:07 +0200

    Fix api

--- src/config/axios.config.ts
+++ src/config/axios.config.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: "http://localhost:1337/api",
+  baseURL: "https://mostafa-todo-project-ts-reactjs-strapi.onrender.com/api",
   timeout: 1000,
 });
 
