a6a7b8780328c6b0073847418bac74d7a0cc9078
mostafasasa010 <mostafa.sasa20004@gmail.com>
2024-03-21 07:27:05 +0200

    change api

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -26,40 +26,42 @@ const Navbar = () => {
   };
 
   return (
-    <nav className="max-w-2xl mx-auto mt-2 mb-6 px-3 py-2 bg-gray-100 rounded-md">
-      <ul className="flex items-center justify-between">
-        <li className="duration-200 font-semibold text-lg text-gray-700">
-          <NavLink to="/">Home</NavLink>
-        </li>
-        {userData ? (
-          <div className="flex items-center space-x-6">
-            {userData?.user?.email === admin && (
+    <nav className="container">
+      <div className="max-w-2xl mx-auto mt-2 mb-6 px-3 py-2 bg-gray-100 rounded-md">
+        <ul className="flex items-center justify-between">
+          <li className="duration-200 font-semibold text-lg text-gray-700">
+            <NavLink to="/">Home</NavLink>
+          </li>
+          {userData ? (
+            <div className="flex items-center space-x-2 md:space-x-4">
+              {userData?.user?.email === admin && (
+                <li className="duration-200 text-lg font-semibold">
+                  <NavLink to="/todos">Todos</NavLink>
+                </li>
+              )}
               <li className="duration-200 text-lg font-semibold">
-                <NavLink to="/todos">Todos</NavLink>
+                <NavLink to="/profile">Profile</NavLink>
               </li>
-            )}
-            <li className="duration-200 text-lg font-semibold">
-              <NavLink to="/profile">Profile</NavLink>
-            </li>
-            <Button
-              className="font-semibold cursor-pointer"
-              size={"sm"}
-              onClick={onLogout}
-            >
-              Logout
-            </Button>
-          </div>
-        ) : (
-          <p className="flex items-center space-x-3">
-            <li className="duration-200 font-semibold text-lg">
-              <NavLink to="/register">Register</NavLink>
-            </li>
-            <li className="duration-200 font-semibold text-lg">
-              <NavLink to="/login">Login</NavLink>
-            </li>
-          </p>
-        )}
-      </ul>
+              <Button
+                className="font-semibold cursor-pointer"
+                size={"sm"}
+                onClick={onLogout}
+              >
+                Logout
+              </Button>
+            </div>
+          ) : (
+            <p className="flex items-center space-x-3">
+              <li className="duration-200 font-semibold text-lg">
+                <NavLink to="/register">Register</NavLink>
+              </li>
+              <li className="duration-200 font-semibold text-lg">
+                <NavLink to="/login">Login</NavLink>
+              </li>
+            </p>
+          )}
+        </ul>
+      </div>
     </nav>
   );
 };
--- src/config/axios.config.ts
+++ src/config/axios.config.ts
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  // baseURL: "https://proud-captain-d5ba775f78.strapiapp.com/api",
-  baseURL: "http://localhost:1337/api",
+  baseURL: "https://proud-captain-d5ba775f78.strapiapp.com/api",
+  // baseURL: "http://localhost:1337/api",
   // baseURL: "https://mostafa-todo-project-ts-reactjs-strapi.onrender.com/api",
   timeout: 1000,
 });
