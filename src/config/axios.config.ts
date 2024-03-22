f557b392cfe260cd8c4b16c5f1cd9981d4c96a97
mostafasasa010 <mostafa.sasa20004@gmail.com>
2024-03-22 23:26:12 +0200

    fix timeout api

--- src/config/axios.config.ts
+++ src/config/axios.config.ts
@@ -4,7 +4,7 @@ const axiosInstance = axios.create({
   // baseURL: "https://proud-captain-d5ba775f78.strapiapp.com/api",
   // baseURL: "http://localhost:1337/api",
   baseURL: "https://todo-strapi-new.onrender.com/api",
-  timeout: 1000,
+  timeout: 5000,
 });
 
 export default axiosInstance;
